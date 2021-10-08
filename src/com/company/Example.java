38d860fc2071ae69c226e2d306be10e2d7182d8f
japon <japo0n@mail.ru>
2021-10-09 02:43:30 +0600

    Initial commit

---
+++ src/com/company/Example.java
@@ -0,0 +1,31 @@
+package com.company;
+
+// Найти факториал N-го числа
+
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Random;
+
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.print("N = ");
+        int n = in.nextInt();
+
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            fact *= i;
+
+        }
+        System.out.println(n + "! = " + fact);
+
+        return;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+// Условие Задачи
+
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+            
+            //Решение Задачи
+
+    }
+}
+
